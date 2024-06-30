af53594fedcaf95391d20514b643a18dce3c1da7
Alex <58037533+Creep-ai@users.noreply.github.com>
2024-06-30 12:27:51 +0000

    add elasticsearch demo for rag

--- module_1/utils.py
+++ module_1/utils.py
@@ -1,4 +1,5 @@
 import minsearch
+from elasticsearch import Elasticsearch
 from groq import Groq
 
 
@@ -7,6 +8,24 @@ def search(index: minsearch.Index, query: str, num_results: int = 10) -> list[di
     return index.search(
         query=query, filter_dict={"course": "data-engineering-zoomcamp"}, boost_dict=boost, num_results=num_results
     )
+
+
+def elastic_search(
+    query: str, elastic_search_client: Elasticsearch, index_name: str, num_results: int = 5
+) -> list[dict[str, str]]:
+    search_query = {
+        "size": num_results,
+        "query": {
+            "bool": {
+                "must": {
+                    "multi_match": {"query": query, "fields": ["question^3", "text", "section"], "type": "best_fields"}
+                },
+                "filter": {"term": {"course": "data-engineering-zoomcamp"}},
+            }
+        },
+    }
+    elastic_search_results = elastic_search_client.search(index=index_name, body=search_query)
+    return [one_hit["_source"] for one_hit in elastic_search_results["hits"]["hits"]]
 
 
 def build_prompt(query: str, search_results: list[dict[str, str]]) -> str:
@@ -40,7 +59,17 @@ def llm(client: Groq, prompt: str) -> str | None:
     return chat_completion.choices[0].message.content
 
 
-def rag(client: Groq, index: minsearch.Index, question: str) -> str | None:
+def rag_minsearch(client: Groq, index: minsearch.Index, question: str) -> str | None:
     search_results = search(index, question, num_results=5)
+    prompt = build_prompt(question, search_results)
+    return llm(client, prompt)
+
+
+def rag_elastic(
+    client: Groq, elastic_search_client: Elasticsearch, index_name: str, num_results, question: str
+) -> str | None:
+    search_results = elastic_search(
+        query=question, elastic_search_client=elastic_search_client, index_name=index_name, num_results=num_results
+    )
     prompt = build_prompt(question, search_results)
     return llm(client, prompt)
